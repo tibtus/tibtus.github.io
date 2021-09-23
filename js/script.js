d02cdc9e546e62abcd7755d035cd0cb7a4f830d6
TibTus <tibtus@gmail.com>
2021-09-23 13:52:42 +0300

    #New_files_project_portfolio_edit_23_09_21

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+
+});
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});    
